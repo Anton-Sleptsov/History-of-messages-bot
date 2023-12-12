4ffcf183c5e4e2ede2a079c81532b7085625a349
Anton-Sleptsov <arslepcov@kait20.ru>
2023-12-12 23:59:00 +0300

    Создано подключение к БД. Бот работает только в конкретном чате.
    Шаблон подключения переделан под MySql. Добавлено подключение к тестовой базе данных.
    Добавлена поддержка только конкретного чата в телеграме. Если бот добавлен в другой чат, он отправляет ссылку на гитхаб.

--- History of messages bot/Program.cs
+++ History of messages bot/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types;
-using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 
 namespace History_of_messages_bot
 {
@@ -9,8 +9,10 @@ namespace History_of_messages_bot
     {
         private static string _token { get; set; } = "6351710759:AAFcfAOI0pATZc3s4ggHdOUw3wnaRmSNKO0";
         private static TelegramBotClient _client;
+        private static long _chatId = -1002129394383;
 
-        private static string connectionString = "your_connection_string";
+       private static MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root;password=;" +
+           "database=History_of_messages");
 
         static void Main(string[] args)
         {
@@ -24,7 +26,7 @@ namespace History_of_messages_bot
         private static async void OnMessageHandler(object? sender, MessageEventArgs e)
         {
             Message message = e.Message;
-            if (message.Text != null && message.Chat.Type != Telegram.Bot.Types.Enums.ChatType.Private)
+            if (message.Text != null && message.Chat.Type != Telegram.Bot.Types.Enums.ChatType.Private && message.Chat.Id == _chatId)
             {
                 var chatId = message.Chat.Id;
                 string text = message.Text;
@@ -35,31 +37,45 @@ namespace History_of_messages_bot
                 Console.WriteLine($"{date} Из чата номер {chatId} с названием \"{title}\" пришло сообщение от пользователя {userName}, " +
                     $" вот его текст \"{text}\"");
 
-                SaveMessageToDatabase(chatId, text, userName, date);
+                SaveMessageToDatabase(text, userName, date);
             }
             else if (message.Chat.Type == Telegram.Bot.Types.Enums.ChatType.Private)
             {
                 await _client.SendTextMessageAsync(message.Chat.Id, $"Добавьте бота в группу, чтобы он сохранял все сообщения из неё");
             }
+            else if (message.Chat.Id != _chatId)
+            {
+                await _client.SendTextMessageAsync(message.Chat.Id, $"К сожалению, данный бот создан только для служебного пользования, " +
+                    $"но его исходный код можно посмотреть на гитхабе: https://github.com/Anton-Sleptsov/History-of-messages-bot.git " +
+                    $"\nУдалите его из группы");
+            }
         }
 
-        private static void SaveMessageToDatabase(long chatId, string text, string userName, DateTime date)
+        private static void SaveMessageToDatabase(string text, string userName, DateTime date)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+
+            try
             {
                 connection.Open();
 
-                string query = "INSERT INTO YourTableName (ChatId, Text, UserName, Date) VALUES (@ChatId, @Text, @UserName, @Date)";
+                string query = "INSERT INTO History_in_group (Text, UserName, Date) VALUES (@Text, @UserName, @Date)";
 
-                using (SqlCommand command = new SqlCommand(query, connection))
+                using (MySqlCommand command = new MySqlCommand(query, connection))
                 {
-                    command.Parameters.AddWithValue("@ChatId", chatId);
                     command.Parameters.AddWithValue("@Text", text);
                     command.Parameters.AddWithValue("@UserName", userName);
                     command.Parameters.AddWithValue("@Date", date);
 
                     command.ExecuteNonQuery();
                 }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("При записи в базу что-то пошло нет так, а именно " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
             }
         }
     }
