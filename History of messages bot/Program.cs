4d67aafcf8f7fb59adcc44f249287d231b5c3657
Anton-Sleptsov <arslepcov@kait20.ru>
2023-12-12 13:54:00 +0300

    Добавьте файлы проекта.

---
+++ History of messages bot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace History_of_messages_bot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
