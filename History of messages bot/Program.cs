a2229c5d552d007c4f319ffdfdeae77bac157a8a
Anton-Sleptsov <arslepcov@kait20.ru>
2023-12-12 14:23:59 +0300

    Скачан NuGet для работы с телеграмом. Подключён бот

--- History of messages bot/Program.cs
+++ History of messages bot/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace History_of_messages_bot
+﻿using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+
+namespace History_of_messages_bot
 {
     internal class Program
     {
+        private static string _token { get; set; } = "6351710759:AAFcfAOI0pATZc3s4ggHdOUw3wnaRmSNKO0";
+        private static TelegramBotClient _client;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            _client = new TelegramBotClient(_token);
+            _client.StartReceiving();
+            Console.ReadLine();
+            _client.StartReceiving();
         }
     }
 }
