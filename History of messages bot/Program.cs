de6c0e09e9c299226cce3624202e962948520379
Anton-Sleptsov <arslepcov@kait20.ru>
2023-12-12 19:28:21 +0300

    Добавлен NuGet для работы с базами данных, а также шаблон для записи данных в БД

--- History of messages bot/Program.cs
+++ History of messages bot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types;
+using System.Data.SqlClient;
 
 namespace History_of_messages_bot
 {
@@ -8,6 +9,8 @@ namespace History_of_messages_bot
     {
         private static string _token { get; set; } = "6351710759:AAFcfAOI0pATZc3s4ggHdOUw3wnaRmSNKO0";
         private static TelegramBotClient _client;
+
+        private static string connectionString = "your_connection_string";
 
         static void Main(string[] args)
         {
@@ -31,10 +34,32 @@ namespace History_of_messages_bot
 
                 Console.WriteLine($"{date} Из чата номер {chatId} с названием \"{title}\" пришло сообщение от пользователя {userName}, " +
                     $" вот его текст \"{text}\"");
+
+                SaveMessageToDatabase(chatId, text, userName, date);
             }
             else if (message.Chat.Type == Telegram.Bot.Types.Enums.ChatType.Private)
             {
                 await _client.SendTextMessageAsync(message.Chat.Id, $"Добавьте бота в группу, чтобы он сохранял все сообщения из неё");
+            }
+        }
+
+        private static void SaveMessageToDatabase(long chatId, string text, string userName, DateTime date)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "INSERT INTO YourTableName (ChatId, Text, UserName, Date) VALUES (@ChatId, @Text, @UserName, @Date)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ChatId", chatId);
+                    command.Parameters.AddWithValue("@Text", text);
+                    command.Parameters.AddWithValue("@UserName", userName);
+                    command.Parameters.AddWithValue("@Date", date);
+
+                    command.ExecuteNonQuery();
+                }
             }
         }
     }
