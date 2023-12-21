64c12ddb097f1455090ec96d2aedfdaaf522cc2b
Anton-Sleptsov <arslepcov@kait20.ru>
2023-12-21 23:23:08 +0300

    Добавлен контроллер. Который позволяет получить все сообщения из таблицы в БД.

---
+++ History of messages web/Controllers/AllMessagesFromDatabase.cs
@@ -0,0 +1,58 @@
+﻿using History_of_messages_web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace History_of_messages_web.Controllers
+{
+    public class AllMessagesFromDatabase : Controller
+    {
+        private static readonly MySqlConnection _connection = new MySqlConnection("server=localhost;port=3306;username=root;password=;database=History_of_messages");
+
+        public IActionResult Index()
+        {
+            List<Message> messages = new();
+
+            try
+            {
+                _connection.Open();
+
+                string query = "SELECT * FROM History_in_group";
+                using (MySqlCommand command = new MySqlCommand(query, _connection))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32("Id");
+                            int messageId = reader.GetInt32("MessageId");
+                            string text = reader.GetString("Text");
+                            string userName = reader.GetString("UserName");
+                            DateTime date = reader.GetDateTime("Date");
+                            bool messageIsRelevant = reader.GetBoolean("MessageIsRelevant");
+                            int? originalId = reader.IsDBNull("OriginalId") ? (int?)null : reader.GetInt32("OriginalId");
+
+                            messages.Add(new Message
+                            {
+                                Id = id,
+                                MessageId = messageId,
+                                Text = text,
+                                UserName = userName,
+                                Date = date,
+                                MessageIsRelevant = messageIsRelevant,
+                                OriginalId = originalId
+                            });
+                        }
+                    }
+                }
+            }
+            finally 
+            { 
+                _connection.Close(); 
+            }
+                
+            return View(messages);
+        }
+    }
+}
