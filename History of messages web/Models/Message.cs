1bb7afac596c830eeae75d9acf630c10e0eae91b
Anton-Sleptsov <arslepcov@kait20.ru>
2023-12-21 23:12:50 +0300

    Создана модель, для хранения информации о сообщении

---
+++ History of messages web/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace History_of_messages_web.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public int MessageId { get; set; }
+        public string Text { get; set; }
+        public string UserName { get; set; }
+        public DateTime Date { get; set; }
+        public bool MessageIsRelevant { get; set; }
+        public int? OriginalId { get; set; }
+
+        [ForeignKey("OriginalId")]
+        public Message OriginalEntity { get; set; }
+    }
+}
