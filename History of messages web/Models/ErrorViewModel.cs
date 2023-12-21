d02563c813f7851bdd0b28f5fd7bc17c8718642d
Anton-Sleptsov <arslepcov@kait20.ru>
2023-12-21 23:08:44 +0300

    Создан проект ASP, для вывода истории сообщений на веб страницу

---
+++ History of messages web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace History_of_messages_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
